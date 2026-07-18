6fdcf8facf4569b9846552529b004b826040242f
astridabarcaavila-hash <astridabarcaavila@gmail.com>
2026-07-18 17:44:10 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,65 @@
+
+const CACHE_VERSION = "snackly-mobile-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./offline.html",
+  "./manifest.webmanifest",
+  "./icons/icon-32.png",
+  "./icons/icon-96.png",
+  "./icons/icon-180.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(APP_SHELL))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_VERSION).map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_VERSION).then((cache) => cache.put("./index.html", copy));
+          return response;
+        })
+        .catch(async () => {
+          return (await caches.match(event.request))
+            || (await caches.match("./index.html"))
+            || (await caches.match("./offline.html"));
+        })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        if (response && response.status === 200 && response.type === "basic") {
+          const copy = response.clone();
+          caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      });
+    })
+  );
+});
