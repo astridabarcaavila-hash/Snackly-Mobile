676af7b65142da56e4992d22b7aef9830a8775d7
astridabarcaavila-hash <astridabarcaavila@gmail.com>
2026-07-18 18:28:53 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,65 +1,42 @@
 
-const CACHE_VERSION = "snackly-mobile-v1";
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./offline.html",
-  "./manifest.webmanifest",
-  "./icons/icon-32.png",
-  "./icons/icon-96.png",
-  "./icons/icon-180.png",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/icon-maskable-512.png"
+const CACHE="snackly-v7-interactivo";
+const ASSETS=[
+ "./","./index.html","./offline.html","./manifest.webmanifest",
+ "./icons/icon-32.png","./icons/icon-96.png","./icons/icon-180.png",
+ "./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png"
 ];
 
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_VERSION).then((cache) => cache.addAll(APP_SHELL))
-  );
+self.addEventListener("install",event=>{
+ self.skipWaiting();
+ event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys()
-      .then((keys) => Promise.all(
-        keys.filter((key) => key !== CACHE_VERSION).map((key) => caches.delete(key))
-      ))
-      .then(() => self.clients.claim())
-  );
+self.addEventListener("activate",event=>{
+ event.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+   .then(()=>self.clients.claim())
+ )
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_VERSION).then((cache) => cache.put("./index.html", copy));
-          return response;
-        })
-        .catch(async () => {
-          return (await caches.match(event.request))
-            || (await caches.match("./index.html"))
-            || (await caches.match("./offline.html"));
-        })
-    );
-    return;
-  }
-
+self.addEventListener("fetch",event=>{
+ if(event.request.method!=="GET")return;
+ if(event.request.mode==="navigate"){
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request).then((response) => {
-        if (response && response.status === 200 && response.type === "basic") {
-          const copy = response.clone();
-          caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, copy));
-        }
-        return response;
-      });
-    })
+   fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put("./index.html",copy));
+    return response
+   }).catch(()=>caches.match("./index.html").then(response=>response||caches.match("./offline.html")))
   );
+  return
+ }
+ event.respondWith(
+  caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+   if(response.ok){
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy))
+   }
+   return response
+  }))
+ )
 });
